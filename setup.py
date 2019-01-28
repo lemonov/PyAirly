fe8684d593613e3efa7e70887f5b58d09d941b8d
LemanPio <piotr.lemanczyk@infinit.cl>
2019-01-28 15:54:50 +0100

    Adde package config

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyAirly',
+      version='1.0',
+      description='Python binding of Airly API',
+      author='Piotr Lemańczyk',
+      author_email='piotrlemanczyk1989@gmail.com',
+      url='',
+      install_requires=[
+          'requests',
+      ]
+      )
